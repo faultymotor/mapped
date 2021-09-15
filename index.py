d511057db2d36d0de42fc3c6b4f06a90ce8bb4bd
Rishi Nath <rishi.nath@gmail.com>
2021-09-15 16:22:59 -0700

    display purely random heightmap

---
+++ index.py
@@ -0,0 +1,33 @@
+import pygame
+
+from noise import pnoise2 as pnoise
+import numpy as np
+
+import random
+
+dim = (256, 256)
+
+def create_heightmap(dim, noise):
+    width, height = dim
+
+    def to_filtered_tuple(x, y, z):
+        val = noise(x / width, y / height)
+        val = int(255 * val)
+        return val
+
+    hmap = np.fromfunction(np.vectorize(to_filtered_tuple), (width, height, 3))
+    
+    return hmap.astype('uint8')
+
+hmap = create_heightmap(dim, lambda x, y: random.randint(0, 1))
+
+clock = pygame.time.Clock()
+surf = pygame.surfarray.make_surface(hmap)
+display = pygame.display.set_mode(dim)
+display.blit(surf, (0, 0))
+
+pygame.display.update()
+
+while True:
+    pygame.display.update()
+    clock.tick(60)
