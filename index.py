754773871fc1321d9e076f50e5ffe1e520c38d2c
Rishi Nath <rishi.nath@gmail.com>
2021-09-15 16:58:30 -0700

    proof of concept terrain from heightmap

--- index.py
+++ index.py
@@ -1,19 +1,34 @@
 import pygame
-
 from noise import pnoise2
 import numpy as np
-
+import sys
 import random
 
-dim = (256, 256)
+dim = (1024, 768)
+
+rgbs = [
+    [65, 105, 225], # 0 Water
+    [238, 213, 180], # 1 Beach
+    [168, 211, 171], # 2 Land
+    [159, 146, 134], # 3 Hill
+    [224, 224, 224], # 4 Snow
+]
+
+def get_rgb(height):
+    idx = 0
+    if height > 20: idx = 1
+    if height > 25: idx = 2
+    if height > 70: idx = 3
+    if height > 75: idx = 4
+    return rgbs[idx]
 
 def create_heightmap(dim, noise):
     width, height = dim
 
     def to_filtered_tuple(x, y, z):
         val = noise(x / width, y / height)
-        val = int(255 * val)
-        return val
+        val = int(255 * abs(val))
+        return get_rgb(val)[int(z)]
 
     hmap = np.fromfunction(np.vectorize(to_filtered_tuple), (width, height, 3))
 
@@ -21,8 +36,7 @@ def create_heightmap(dim, noise):
 
 base = np.random.randint(0, 100)
 
-hmap = create_heightmap(dim, lambda x, y: pnoise2(x, y, base=base))
-
+hmap = create_heightmap(dim, lambda x, y: pnoise2(x, y, octaves=8, base=base))
 clock = pygame.time.Clock()
 surf = pygame.surfarray.make_surface(hmap)
 display = pygame.display.set_mode(dim)
