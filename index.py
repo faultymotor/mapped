35bfb75074fd2f09e2ad72298718a19ca594eb74
Rishi Nath <rishi.nath@gmail.com>
2021-09-16 11:30:24 -0700

    map ooped, also cycle modes

--- index.py
+++ index.py
@@ -1,36 +1,37 @@
 import pygame
-import numpy as np
-import sys
-import random
-
 from mapped import biomes, maps
+import numpy as np
 
-def update_screen():
-    print('=== GENERATING NEW MAP ===')
-    bmap = maps.create_normalized_map(dim, octaves=6)
-    hmap = maps.create_normalized_map(dim, octaves=8)
-    cmap = maps.color_map(hmap, lambda x, y: biomes.biome(hmap[x][y], bmap[x][y]))
-
-    surf = pygame.surfarray.make_surface(cmap)
-    display.blit(surf, (0, 0))
+def draw_map_to_screen(nmap, display):
+    surface = pygame.surfarray.make_surface(nmap)
+    display.blit(surface, (0, 0))
 
 dim = (700, 700)
-clock = pygame.time.Clock()
 
+mode = 'color'
+
+clock = pygame.time.Clock()
+ 
 pygame.init()
 pygame.display.set_caption('mapped')
 display = pygame.display.set_mode(dim)
 
-update_screen()
-
 running = True
+
+map_obj = maps.Map(dim)
+draw_map_to_screen(map_obj.dmap, display)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == 768: # pygame.K_TAB doesn't work for some reason?
-            bmap = update_screen()
+        # if event.type == 768: # pygame.K_TAB doesn't work for some reason?
+        #     nmap = create_new_map()
+        #     draw_map_to_screen(nmap, display)
+        if event.type == 768:
+            map_obj.cycle_mode()
 
+    draw_map_to_screen(map_obj.dmap, display)
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
+
--- mapped/maps.py
+++ mapped/maps.py
@@ -1,26 +1,63 @@
 from noise import pnoise2 as pnoise
 import numpy as np
+from mapped import biomes
 
-def create_normalized_map(dim, octaves=6, base=None):
-    width, height = dim
+class Map():
+    def __init__(self, dim=(700, 700), repeatx=1024, repeaty=1024):        
+        width, height = dim
+        self.mode = 'color'
+        self.dim = dim
+        self.dim3 = (width, height, 3)
 
-    if not base: base = np.random.randint(0, 100)
-    mapper = lambda x, y: pnoise(x / width, y / height, octaves=octaves, base=base)
-    mapper = np.vectorize(mapper)
-    nmap = np.fromfunction(mapper, dim)
+        self.create_mapper = lambda base, octaves: np.vectorize(lambda x, y: pnoise(x / width, y / height, base=base, octaves=octaves, repeatx=repeatx, repeaty=repeaty))
 
-    min_val, max_val = np.min(nmap), np.max(nmap)
-    normalize = lambda val: (val - min_val) / (max_val - min_val)
-    normalize = np.vectorize(normalize)
+        self.dmap = np.zeros(self.dim3)
+        self.bmap, self.hmap = Map.create_normalized_map(self.dim, self.create_mapper, 6), Map.create_normalized_map(self.dim, self.create_mapper, 8)
+        self.construct_display_map()
 
-    return normalize(nmap)    
+    def cycle_mode(self):
+        if self.mode == 'color': self.mode = 'height'
+        elif self.mode == 'height': self.mode = 'moisture'
+        else: self.mode = 'color'
 
-def color_map(nmap, colorer):
-    width, height = nmap.shape
-    cmap = np.zeros((width, height, 3))
+        self.construct_display_map()
 
-    for x in range(width):
-        for y in range(height):
-            cmap[x][y] = colorer(x, y)
+    def construct_display_map(self):
+        colorer = None
+        if self.mode == 'color': colorer = lambda x, y: biomes.biome(self.hmap[x][y], self.bmap[x][y])
+        if self.mode == 'height': colorer = Map.create_map_terracer(self.hmap, 16)
+        if self.mode == 'moisture': colorer = Map.create_map_terracer(self.bmap, 12, color=[65, 105, 225])
+
+        print("Creating new display map with mode", self.mode)
+
+        self.dmap = Map.color_map(self.dim3, colorer)
+
+    @staticmethod
+    def create_normalized_map(dim, create_mapper, octaves=6):
+        print("Creating new map with octaves =", octaves)
+        width, height = dim
+
+        base = np.random.randint(0, 100)
+        mapper = create_mapper(base, octaves)
+        nmap = np.fromfunction(mapper, dim)
+
+        min_val, max_val = np.min(nmap), np.max(nmap)
+        normalize = lambda val: (val - min_val) / (max_val - min_val)
+        normalize = np.vectorize(normalize)
+
+        return normalize(nmap)   
+
+    @staticmethod
+    def create_map_terracer(nmap, step, color=[255, 255, 255]):
+        return lambda x, y: [hue * (int(nmap[x][y] * 12) / 12) for hue in color]
+
+    @staticmethod
+    def color_map(dim, colorer):
+        width, height, depth = dim
+        cmap = np.zeros(dim)
+
+        for x in range(width):
+            for y in range(height):
+                cmap[x][y] = colorer(x, y)
     
-    return cmap.astype('uint8')
+        return cmap.astype('uint8')
